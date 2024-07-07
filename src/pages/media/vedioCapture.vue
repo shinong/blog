e122f9d234ad642bccbd2e0751434fc8dcde304a
Shinong <shinongmao@gmail.com>
2024-07-07 18:11:53 -0400

    ignore ts error for github build

--- src/pages/media/vedioCapture.vue
+++ src/pages/media/vedioCapture.vue
@@ -19,6 +19,7 @@ export default defineComponent({
         const constraints = { video: true, audio: true }
         const stream = await navigator.mediaDevices.getUserMedia(constraints)
         const videoElement = document.querySelector('video#localVideo')
+        // @ts-ignore
         videoElement.srcObject = stream
       } catch (error) {
         console.error('Error opening video camera.', error)
